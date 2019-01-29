d65f5338eb319f00c335277f2b9247c59967d8dc
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2019-01-29 17:16:56 -0500

    added foreign key

--- user_items/models.py
+++ user_items/models.py
@@ -10,6 +10,7 @@ import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.gis.db import models
+from users.models import MyUser
 
 
 #class MyUser(AbstractUser):
@@ -28,21 +29,33 @@ from django.contrib.gis.db import models
 #from django.db import models
 
 class UserItem(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField(MyUser, default=uuid.uuid4, editable=False)
     description = models.TextField()
     location = models.PointField(spatial_index=True, geography=True)
     date_posted = models.DateTimeField(default=datetime.datetime.now())
     cost = models.CharField(max_length=10) #giving away
     note = models.TextField()
-#		tags
-#		pickup location
+    #for sale
+#	for rent
+#   sold
+#.  rented  	
+#   tags
+#	pickup location
 
-	meta:
-		db_table: "user_items"
+    class meta:
+      db_table: "user_items"
 
 #>>> eventobject = Event.objects.all()
 #>>> eventobject.first().id
 #'3cd2b4b0c36f43488a93b3bb72029f46'
+
+#UserItem.objects.create(location=Point(lng, lat))
+#from django.contrib.gis.geos import Point
+#from django.contrib.gis.measure import D
+
+#my_loc = Point(my_lng, my_lat)
+#UserItem.objects.filter(location__distance_lte=(my_loc, D(m=5000))
 
 
 #class UserItemTags(AbstractUser):
